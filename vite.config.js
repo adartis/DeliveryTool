9ef1b4f9700d59c8d08d54806a5228fa95115b71
adartis <g.kokkinos87@gmail.com>
2024-11-10 22:54:21 +0100

    Changes to be committed: 	modified:   vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/DeliveryTool/', // Just the repository name with slashes
+  base: '/DeliveryTool/', // This should match the GitHub Pages repository name
 });
