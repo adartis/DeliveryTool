40e116317233ac7b8017f50770eafe559dd70208
adartis <g.kokkinos87@gmail.com>
2024-11-11 20:07:33 +0100

    New html pages

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/DeliveryTool/', // Set this to match the repository name exactly
+  base: '/DeliveryTool/', // Ensure this matches the repo name exactly
 });
